2b7c3e5fe75029343723a157d47088f6aac97a71
rebeca <beca.cout@gmail.com>
2023-08-28 12:32:37 -0300

    initial

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const app = require('express')()
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
+
+io.on('connection', socket => {
+    console.log("Usuário Conectado", socket.id);
+
+    socket.on('disconnect', reason => {
+        console.log('Usuário desconectado'. socket.id);
+    });
+
+    socket.on('set_username', username => {
+        //a propriedade date é do socket e permite guardar diversas informações
+        socket.data.username = username;
+    })
+
+    socket.on('message', text => {
+        io.emit('receive_message', {
+            text,
+            authorId: socket.id,
+            author: socket.data.username
+        })
+    })
+})
