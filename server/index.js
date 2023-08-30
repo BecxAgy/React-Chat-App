48b6b7c5b31d03c116b34701c1a57e183f09af62
rebeca <beca.cout@gmail.com>
2023-08-30 11:49:49 -0300

    Alterando endereçamento

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
+const io = require('socket.io')(server, {cors: {origin: 'https://becarealchatfrontend.onrender.com'}})
 
 const PORT = 3001;
 
